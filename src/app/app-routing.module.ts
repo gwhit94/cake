7b4250ec17e0f3972b18a036b35f524cb1b8a0f5
Graham <greysnaa@gmail.com>
2019-09-16 14:52:24 -0500

    favicon and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'gallery', component: GalleryComponent},
+  // { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: '**', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
