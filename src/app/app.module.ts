baf946f1edc37bc333e22d3332a117bbd7fbe7a3
Graham <greysnaa@gmail.com>
2019-09-16 13:22:21 -0500

    added page components and api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    ContactComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
